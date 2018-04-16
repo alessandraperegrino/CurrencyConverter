00d2a0f551480681ea502cd58c269c3c1c06fa19
alessandraperegrino <alessandraperegrino@github.com>
2018-04-16 16:11:25 -0300

    Tela de temas

--- app/components/List/ListItem.js
+++ app/components/List/ListItem.js
@@ -5,14 +5,25 @@ import { View, Text, TouchableHighlight } from 'react-native';
 import styles from './styles';
 import Icon from './Icon';
 
-const ListItem = ({ text, onPress, checkmark = true, selected = false, visible = true }) => (
-  <TouchableHighlight onPress={onPress} underlayColor={styles.$underlayColor}>
-    <View style={styles.row}>
-      <Text style={styles.text}>{text}</Text>
-      {selected ? <Icon visible={visible} checkmark={checkmark} /> : <Icon />}
-    </View>
-  </TouchableHighlight>
-);
+const ListItem = ({
+  text,
+  onPress,
+  checkmark = true,
+  selected = false,
+  visible = true,
+  customIcon = null,
+  iconBackground,
+}) => (
+    <TouchableHighlight onPress={onPress} underlayColor={styles.$underlayColor}>
+      <View style={styles.row}>
+        <Text style={styles.text}>{text}</Text>
+        {selected
+          ? <Icon visible={visible} checkmark={checkmark} iconBackground={iconBackground} />
+          : <Icon />}
+        {customIcon}
+      </View>
+    </TouchableHighlight>
+  );
 
 ListItem.propTypes = {
   text: PropTypes.string,
@@ -20,6 +31,8 @@ ListItem.propTypes = {
   checkmark: PropTypes.bool,
   selected: PropTypes.bool,
   visible: PropTypes.bool,
+  customIcon: PropTypes.element,
+  iconBackground: PropTypes.string,
 };
 
 export default ListItem;
